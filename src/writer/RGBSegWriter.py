2d1d35d7d6579e642531d416437fe0ad16adcbc7
Rex <hkchengrex@gmail.com>
2021-01-14 16:36:57 +0800

    should work but I don't have the time/res to run it

--- src/writer/RGBSegWriter.py
+++ src/writer/RGBSegWriter.py
@@ -28,16 +28,21 @@ class RGBSegWriter(Module):
         self.rgb_output_key = self.config.get_string("rgb_output_key", "colors")
         self.segmap_png_output_key = self.config.get_string("segmap_png_output_key", "segmap_png")
 
+        self.fw_flow_output_key = self.config.get_string("forward_flow_output_key", "forward_flow")
+        self.bw_flow_output_key = self.config.get_string("backward_flow_output_key", "backward_flow")
+
         base_path = self._determine_output_dir(False)
         base_folder = os.path.dirname(base_path)
         vid_name = os.path.basename(base_path)
         self.ren_out_data_dir = os.path.join(base_folder, 'JPEGImages', vid_name)
         self.seg_out_data_dir = os.path.join(base_folder, 'Annotations', vid_name)
+        self.flw_fw_out_data_dir = os.path.join(base_folder, 'ForwardFlows', vid_name)
+        self.flw_bw_out_data_dir = os.path.join(base_folder, 'BackwardFlows', vid_name)
 
         os.makedirs(self.ren_out_data_dir, exist_ok=True)
         os.makedirs(self.seg_out_data_dir, exist_ok=True)
-        print(self.ren_out_data_dir)
-        print(self.seg_out_data_dir)
+        os.makedirs(self.flw_fw_out_data_dir, exist_ok=True)
+        os.makedirs(self.flw_bw_out_data_dir, exist_ok=True)
 
     def run(self):
         if self._avoid_rendering:
@@ -53,12 +58,22 @@ class RGBSegWriter(Module):
         rgb_output = self._find_registered_output_by_key(self.rgb_output_key)
         if rgb_output is None:
             raise Exception("There is no output registered with key " + self.rgb_output_key + ". Are you sure you ran the SimRgbRenderer module before?")
+
+        # Find path pattern of flows
+        fw_flow_output = self._find_registered_output_by_key(self.fw_flow_output_key)
+        if fw_flow_output is None:
+            raise Exception("There is no output registered with key " + self.fw_flow_output_key + ". Are you sure you ran the SimFlowRenderer module before?")
+
+        # Find path pattern of flows
+        bw_flow_output = self._find_registered_output_by_key(self.bw_flow_output_key)
+        if bw_flow_output is None:
+            raise Exception("There is no output registered with key " + self.bw_flow_output_key + ". Are you sure you ran the SimFlowRenderer module before?")
     
         # collect all segmaps
-        segmentation_map_paths = []
+        # segmentation_map_paths = []
 
         # collect all RGB paths
-        new_out_paths = []
+        # new_out_paths = []
         # for each rendered frame
         for frame in range(bpy.context.scene.frame_start, bpy.context.scene.frame_end):
 
@@ -70,16 +85,22 @@ class RGBSegWriter(Module):
             # Segmentation
             source_path = segmentation_map_output["path"] % frame
             target_path = os.path.join(self.seg_out_data_dir, os.path.basename(segmentation_map_output["path"].replace('%04d','%05d') % (frame)))
-            print(frame, source_path, target_path)
-
             shutil.copyfile(source_path, target_path)
-            segmentation_map_paths.append(segmentation_map_output["path"] % frame)
+            # segmentation_map_paths.append(segmentation_map_output["path"] % frame)
 
             # RGB
             source_path = rgb_output["path"] % frame
             target_path = os.path.join(self.ren_out_data_dir, os.path.basename(rgb_output["path"].replace('%04d','%05d') % (frame)))
-            print(frame, source_path, target_path)
-
             shutil.copyfile(source_path, target_path)
-            new_out_paths.append(os.path.basename(target_path))
+            # new_out_paths.append(os.path.basename(target_path))
+
+            # Flow
+            source_path = fw_flow_output["path"] % frame
+            target_path = os.path.join(self.flw_fw_out_data_dir, os.path.basename(fw_flow_output["path"].replace('%04d','%05d') % (frame)))
+            shutil.copyfile(source_path, target_path)
+
+            source_path = bw_flow_output["path"] % frame
+            target_path = os.path.join(self.flw_bw_out_data_dir, os.path.basename(bw_flow_output["path"].replace('%04d','%05d') % (frame)))
+            shutil.copyfile(source_path, target_path)
+            # new_out_paths.append(os.path.basename(target_path))
 
